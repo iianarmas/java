c15c5c212c9ac72a38a90ef148c3987d9d79ee51
iian armas <armas.cav@gmail.com>
2021-09-19 15:13:39 +0800

    Create TernaryOperator.java

---
+++ TernaryOperator.java
@@ -0,0 +1,19 @@
+
+public class TernaryOperator
+{
+	
+	// ternary operator --> ?:
+	// condition?true:false
+  
+	public static void main(String[] args)
+	{
+		int i = 8, j = 0;
+		
+    // if i is greater than 6, j will become 1, else j will become 2
+    
+		j = i > 6 ? 1 : 2;
+		
+		System.out.println(j);
+		
+	}
+}
