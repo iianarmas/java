84873736fa73be07046e92067e2139e639a18e55
iian armas <armas.cav@gmail.com>
2021-09-19 14:37:35 +0800

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,19 @@
+public class Demo
+{
+	
+	/*
+	 * 
+	 * Arithmetic
+	 * Bitwise
+	 * Relational
+	 * Logical
+	 * 
+	 */
+	public static void main(String[] args)
+	{
+		int m = 6, n =2;
+		int r1 = m + n;
+		
+		System.out.println(r1);
+	}
+}
