9439770eb69bb42a284d148deb2807540613b01b
iian armas <armas.cav@gmail.com>
2021-09-19 14:39:13 +0800

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -13,7 +13,13 @@ public class Demo
 	{
 		int m = 6, n =2;
 		int r1 = m + n;
+		int r2 = m - n;
+		int r3 = m * n ;
+		int r4 = m / n;
 		
 		System.out.println(r1);
+		System.out.println(r2);
+		System.out.println(r3);
+		System.out.println(r4);
 	}
 }
